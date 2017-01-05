25e6d712279baad203ba1a82eab2127b285856fc
xavier.lis <xavier.lis@gmail.com>
2017-01-05 18:18:23 +0300

    Add main app

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
-    'script!foundation-sites/dist/foundation.min.js',
+    'script!foundation-sites/dist/js/foundation.min.js',
     './app/app.jsx'
   ],
   externals: {
@@ -21,6 +21,10 @@ module.exports = {
   },
   resolve: {
     root: __dirname,
+    modulesDirectories: [
+        'node_modules',
+        './app/components'
+    ],
     alias: {
       applicationStyles: 'app/styles/app.scss'
     },
